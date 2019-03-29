c0a78c22e14d75957b2eeabca2c0319d8ed14c71
Yulia51188 <sun51188@gmail.com>
2019-03-29 11:21:49 +0300

    Create cola_monitor.py

---
+++ cola_monitor.py
@@ -0,0 +1,11 @@
+import requests
+from dotenv import load_dotenv
+def main():
+    load_dotenv()
+    service_key = os.getenv("SERVICE_KEY")
+    access_token = os.getenv("ACCESS_TOKEN")    
+    vk_api_version = os.getenv("VERSION")
+
+
+if __name__ == '__main__':
+    main()
